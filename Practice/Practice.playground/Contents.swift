3ca968f1bb1e3e106038b934fc3302d122e5793b
klintukhov <76727787+klintukhov@users.noreply.github.com>
2023-01-28 21:50:59 +0100

    Update Contents.swift

--- Practice/Practice.playground/Contents.swift
+++ Practice/Practice.playground/Contents.swift
@@ -2205,6 +2205,9 @@ testingArray.forNames(namesList: myArray)
     
 import UIKit
 
+
+import UIKit
+
 struct Observer {
     
     var name: String {
@@ -2229,13 +2232,6 @@ obserb.name // getter - vuzov svoystva
 obserb.name = "iVan" // setter - izmenenije svoystva
 
 print(obserb.name )
-
-
-
-
-
-
-
 
 
 // practice from Swift book
@@ -2304,11 +2300,13 @@ print(MyStruct.count)
 
 struct1
 
+
+
 class AgeBoy {
     static var maxAge = 8
     
     lazy var say = "Hello Iam OK)" // lenivaya peremennaya
-        
+    
     var name = String(){
         didSet {
             if name.count > MaxLenghth {
@@ -2373,22 +2371,28 @@ compProp.lastName = "weeeK"
 print(compProp.fullName)
 compProp.fullName
 
+
 //3. Написать структуру "CreateTriangle",с двумя свойствами - угол A,угол C.
 //И создать 2 вычисляемых свойства - те же угол А,угол С.
 //И если мы записываем значения в эти углы - результатом должно быть значение третьего угла.
 //Для простоты можно взять прямоугольный треугольник, сумма углов которого равна 180 градусов.
 
-
-
+let minValue = 0
 
 struct CreateTriangle {
     
-    let angelA = 90
     let allAngels = 180
     
+    var angelA: Int {
+        didSet {
+            if angelA <= minValue {
+                angelA = 1
+            }
+        }
+    }
     var angelB: Int {
         didSet {
-            if angelB < 1 {
+            if angelB <= 0 {
                 print("enter correct data")
             }
             
@@ -2402,11 +2406,170 @@ struct CreateTriangle {
     
 }
 
-var valueC = CreateTriangle(angelB: 30)
+var valueC = CreateTriangle(angelA: 35, angelB: 45)
 valueC.angleC
 print(valueC.angleC)
 
+//4. Создать структуру "Резюме", у которой есть такие свойства:
+//- Фамилия,
+//- Имя.
+//- Должность,
+//- Опыт,
+//- Контактные данные(телефон, емейл),
+//- О себе(можно записать какие-то пару предложений на выбор).
+
+struct Resume {
     
+    var surname: String {
+        didSet {
+            if name != "" {
+                name = name.capitalized
+            }
+        }
+    }
+    var name: String {
+        didSet {
+            if name != "" {
+                name = name.capitalized
+            }
+        }
+    }
+    var position: String {
+        didSet {
+            if position != "" {
+                position = position.capitalized
+            }
+        }
+    }
+    var experience: String {
+        didSet {
+            if position != "" {
+                position = position.capitalized
+            }
+        }
+    }
+    var contacts: String  {
+        didSet {
+            if contacts != "" {
+                contacts = contacts.capitalized
+            }
+        }
+    }
+    var aboutYou: String {
+        didSet {
+            if aboutYou != "" {
+                aboutYou = aboutYou.capitalized
+            }
+        }
+    }
+}
+
+var myResume = Resume(surname: "Ivanov", name: "Semen", position: "Kotlin dev", experience: "one year", contacts: "Semen123@gmail.com", aboutYou: "I like this game")
+myResume.name = "nikita"
+myResume.position = "iOS dev"
+print(myResume)
+
+
+
+//5. Задание на закрепление предыдущих тем:
+//Создать журнал, что б можно было писать имя, фамилию и оценку
+//Поставить ограничения: имя и фамилия не выше 15 символов каждое.
+//И оценка не выше 5.
+//Если везде значения превышаться выдавать об этом Сообщения в консоль.
+
+struct JournalStudents {
+    
+    static var maxNameSurname = 15
+    
+    static var maxMark = 5
+    
+    var surnameStudent: String {
+        didSet {
+            if surnameStudent.count > JournalStudents.maxNameSurname {
+                surnameStudent = ("Enter corrected data")
+                
+            }
+        }
+    }
+    
+    var nameStudent: String {
+        didSet {
+            if nameStudent.count > JournalStudents.maxNameSurname {
+                nameStudent = ("Enter corrected data")
+                
+            }
+        }
+    }
+    
+    var markStudent: Int {
+        didSet {
+            if markStudent > JournalStudents.maxMark {
+                markStudent = oldValue
+                print("sdcscd")
+            }
+        }
+    }
+    
+}
+
+var resultJournal = JournalStudents(surnameStudent: "Week", nameStudent: "Johnnnnnnnnnnnnnnnnnnnnn", markStudent: 4)
+resultJournal.surnameStudent = "Week"
+resultJournal.nameStudent = "Johnnnnnnnnnnnnnnnnnnnnn"
+resultJournal.markStudent = 333
+
+print(resultJournal)
+
+
+
+//6. Написать класс,в котором есть 4 функции:
+//-пустая, которая просто выводит сообщение через print,
+//- которая принимаете параметры и выводит их в консоль,
+//- которая принимает и возвращает параметры.
+//- которая принимает замыкание и распечатывает результаты в консоль
+
+
+class Functions {
+    
+    func funcOne() {
+        print("Just empty message")
+    }
+    
+    func funcTwo(paramOne: String, paramTwo: Int) {
+        print(paramOne, paramTwo)
+    }
+    
+    func funcThree(paramOne: String, paramTwo: String, paramThree: Int) -> (String) {
+        
+        return String(paramOne) + String(paramTwo) + String(paramThree)
+    }
+    func funcFour(for sort: [Int]) -> [Int] {       //////////// ne rabotaet!!!!!!!!
+        
+        var myArray = [Int]()
+        
+        return myArray.sorted(by: {$0 < $1})
+        
+    }
+}
+
+
+var resultFunc = Functions()
+
+resultFunc.funcOne()
+
+resultFunc.funcTwo(paramOne: "Your number", paramTwo: 15)
+
+var myResult = resultFunc.funcThree(paramOne: "BMW ", paramTwo: "525 ", paramThree: 2017)
+print(myResult)
+
+
+var arrayForSort = [9, 4, 1, 5, 7]
+
+var sorting = Functions()
+sorting.funcFour(for: arrayForSort)
+print(arrayForSort)
+var c = sorting.funcFour(for: arrayForSort)
+print(c)
+
     
     
     
