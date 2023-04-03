e6e1675eb8f380e0ce2188bf4cbae3a1fe452c75
klintukhov <76727787+klintukhov@users.noreply.github.com>
2023-04-03 18:11:50 +0200

    Update Contents.swift

--- Practice/Practice.playground/Contents.swift
+++ Practice/Practice.playground/Contents.swift
@@ -3657,9 +3657,14 @@ Extension
 Extension    
     
     
-    import UIKit
+import UIKit
 
-var a = String()
+
+import Darwin
+
+
+
+let a = String()
 
 extension String {
     
@@ -3668,11 +3673,11 @@ extension String {
     }
 }
 
-var hello = "Hi"
+let hello = "Hi"
 
 hello.sayHello()
 
-var b = String()
+let b = String()
 b.sayHello()
 
 extension Int {
@@ -3703,7 +3708,7 @@ class Vasia {
     var nickName = "IOS developer"
 }
 
-var realVasia = Vasia()
+let realVasia = Vasia()
 realVasia.nickName
 
 extension Vasia {
@@ -3714,78 +3719,178 @@ extension Vasia {
     }
 }
 
-var realVasia2 = Vasia()
+let realVasia2 = Vasia()
 realVasia.allInfo()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 // 3)Расширить String, чтобы принимал сабскрипт вида s[0..<3] и мог также устанавливать значения используя его
 
+extension String {
+    
+    subscript(speed: Range<Int>) -> String {
+        return("\(speedCity)\(speed.startIndex) \(speed.endIndex)")
+        }
+}
 
-
-
-
-
+let speedCity = "Max speed in the city is - "
+print("Max speed in the city is - \(speedCity)" [0..<60])
+print(speedCity [0..<60])
 
 
 // 4) Расширить Int и добавить 3 метода возведение в квадрат, куб, степень
 
-class Calculate {
-    var square: Int
-    var kvadrat: Int
-    init(square: Int, kvadrat: Int) {
-    self.square = square
-        self.kvadrat = kvadrat
-
+// v.1
+extension Int {
     
-   
+    func intSquare() -> Int {
+        return(self * self)
     }
-}
     
-extension Calculate {
     
-    func square1(sideA: Int, sideB: Int) -> Int {
-        return(sideA * sideB)
-        }
-    func kvadrat1() -> Int {
-        return(self.kvadrat * self.kvadrat)
+    func intCube() -> Int {
+        return(self * self * self)
+    }
+    
+    func decimal() -> Int {
+        return(NSDecimalNumber(decimal: pow(Decimal(self), self)).intValue)
         
     }
 }
-var calk = Calculate(square: 12, kvadrat: 3)
-calk.square1(sideA: 2, sideB: 4)
 
+var myInt = 4
 
+myInt.intSquare()
+myInt.intCube()
+myInt.decimal()
 
+// v.2
 
+class Calculate {
+    var number = Int()
+    init(number: Int) {
+        self.number = number
+        
+    }
+}
 
-
-
-
+extension Calculate {
+    
+    func square() -> Int {
+        return(self.number * self.number)
+    }
+    
+    func cube() -> Int {
+        return(self.number * self.number * self.number)
+    }
+    
+    func decimal() -> Int {
+        return(NSDecimalNumber(decimal: pow(Decimal(self.number), self.number)).intValue)
+        
+    }
+}
+var calk = Calculate(number: 4)
+calk.square()
+calk.cube()
+calk.decimal()
 
 // 5)Расширить еще 3 структуры на свой вкус и в коментах обосновать для чего это было сделано
+
+// calculating meters to milimeters
+
+var meter: Int?
+
+extension Int {
+    
+    func milimeters() -> Int {
+        
+        return(self * 1000)
+    }
+}
+
+var forCalculate = 28
+
+forCalculate.milimeters()
+
+meter = 10.milimeters()
+
+
+// calculating consumption of fuel in car (for car with consumption 7.5 ltr / 100 km)
+
+var distance: Double?
+
+extension Double {
+    
+    func calculateConsumption() -> Double {
+        return ((self * 7.5) / 100)
+    }
+    
+    func printKm() -> String {
+        return("You need \(calculateConsumption()) liters for distance \(self.distance)")
+    }
+}
+
+distance = 1700
+distance?.calculateConsumption()
+distance?.printKm()
+
+
+// converter currency
+
+var dollarUsa = 100.00
+var rub = 5000.00
+
+extension Double {
+    
+    func currencyRub() -> Double {
+        return(dollarUsa * 78)
+    }
+    
+    func currencyDoll() -> Double {
+        return(rub / 78)
+        
+    }
+}
+dollarUsa = 150
+rub = 5000
+
+dollarUsa.currencyRub()
+rub.currencyDoll()
+
+
 // 6)Добавьте проперти, которое возвращает количество символов в числе
+
+extension Int {
+    
+    func digitsCount() -> Int {
+        return String(self).count
+    }
+}
+
+var digit = 532323
+digit.digitsCount()
+
+
 // 7)Добавить метод который говорит число положительное или отрицательное
 
-    
-    
-    
-    
-    
 
+extension Int {
     
-    
-    
-    
+    func whichNumber() {
+        
+        if self == 0 {
+            print("Entered number is zero")
+            
+        } else if self < 0 {
+            
+            print("the number < 0")
+            
+        }   else {
+            
+            print("Entered number  > 0")
+        }
+    }
+}
+
+let num = 332
+num.whichNumber()
+
+
